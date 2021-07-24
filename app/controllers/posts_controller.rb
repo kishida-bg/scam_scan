1ead96488867c445bcd65e43364f0bfb31ab48de
kishida-bg <kishida@big-gate.co.jp>
2021-07-24 18:46:57 +0900

    add: post#new, #index修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     # このインスタンス変数がviewで参照できる
     @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
   end
 
   def show
@@ -14,5 +19,11 @@ class PostsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :desc)
   end
 end
