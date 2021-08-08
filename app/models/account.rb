05bc493511ec5d18fa47fdea011beca11dbe49d9
kishida-bg <kishida@big-gate.co.jp>
2021-08-08 19:51:12 +0900

    mod: enumの設定

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,8 @@
 class Account < ApplicationRecord
   has_many :comments
 
+  enum sns: { Instagram: 1, Twitter: 2, LINE: 3 }
+
   def self.search(username, sns)
     if username && sns
       account = Account.find_or_initialize_by(username: username, sns: sns)
