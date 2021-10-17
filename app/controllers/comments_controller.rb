a78edb6c36ad64cb6ed6ec313dcccce63d070ec4
kishida-bg <kishida@big-gate.co.jp>
2021-10-17 14:33:47 +0900

    Add: flash message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,11 @@ class CommentsController < ApplicationController
     account = Account.find(params[:comment][:account_id])
     account.comments.build(comment_params)
     if account.save
-      redirect_to root_path
+      redirect_to root_path, notice: "投稿に成功しました"
       # 現在のshowページに遷移するときはaccount_pathの後にパラメータを指定しないといけない
       # 成功したとき、（POSTメソッドの時　など）redirect_to
     else
-      render root
+      render root, alert: '投稿に失敗しました'
       # 失敗ならrender
     end
   end
