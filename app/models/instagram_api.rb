60125098a1d900f28df4839f86ce8be9cb254138
kishida-bg <kishida@big-gate.co.jp>
2021-11-03 21:11:38 +0900

    Review: ローカル変数だからメモリ領域には返り値しか入らないので||いらない

--- app/models/instagram_api.rb
+++ app/models/instagram_api.rb
@@ -1,7 +1,7 @@
 class InstagramApi < ApplicationRecord
   class << self
     def obtain_sns_obj(username)
-      doc ||= Nokogiri::HTML(URI.open("https://dumpor.com/v/#{username.strip}"))
+      doc = Nokogiri::HTML(URI.open("https://dumpor.com/v/#{username.strip}"))
 
       {
         "img": img(doc),
