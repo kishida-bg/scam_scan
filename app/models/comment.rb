893ce0366aebe48808258d923f2b27437413d63c
kishida-bg <kishida@big-gate.co.jp>
2021-11-07 19:38:41 +0900

    Add: 口コミに対して感情分析を行えるようにする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  include GoogleCloudPlatformAccessAuthentication
+
   belongs_to :account
+  before_save :save_sentiment_analysis
+
+  def save_sentiment_analysis
+    client = Authorization.init
+
+    body = read_attribute(:body)
+    document = { content: body, type: :PLAIN_TEXT }
+    response = client.analyze_sentiment document: document
+
+    sentiment = response.document_sentiment
+
+    score = sentiment.score.to_f.round(1)
+    magnitude = sentiment.magnitude.to_f.round(1)
+
+    write_attribute(:score, score)
+    write_attribute(:magnitude, magnitude)
+  end
 end
